acf3a94fb9db5b76ab2e8e7d0813c2e6045c55f0
AleenaPoulose <91655836+AleenaPoulose@users.noreply.github.com>
2021-10-06 12:29:08 +0530

    Add files via upload

--- product.java
+++ product.java
@@ -1,105 +1,94 @@
 import java.io.*;
+import java.util.scanner;
+class product
+{
+int p_code;
+string p_nama;
+int price;
 
-import java.util.Scanner;
-
-class Product2
-
-{ 
- 
-int pcode;
- 
-String pname;
- 
-Float price;
- 
- 
 void read() throws IOException
- 
 {
-   
- System.out.print("\n");
-  
- Scanner po = new Scanner(System.in);
-  
- System.out.print("Enter the Product code: ");
-		
-  
- pcode=po.nextInt();
-   
- System.out.print("Enter the product name: ");
-		
-  
- pname=po.next();
-   
- System.out.print("Enter the product price: ");
-		
-  
- price=po.nextFloat();
-   
- 
+scanner po = new scanner(system.in);
+system.out.print("\n Enter the product code:" );
+int p_code = po.nextInt();
+system.out.print("\n Enter the product name:");
+string p_name = po.next();
+system.out.print("\n Enter the price:");
+int price = po.nextInt();
 }
- 
-void Display() throws IOException
- 
+
+void display() throws IOException
 {
-  
- System.out.print("Product code: " + pcode);
-  
- System.out.print("Name of the Product: " + pname);
-   
- System.out.print("Price of the product: " + price);
-  
-  
+system.out.print("\n The product code is:" +p_code);
+system.out.print("\n The product name is:" + p_name);
+system.out.print("\n The product price is:"+price);
 }
 
-public static void main(String args[])throws IOException
-
+public static void main string (argu[]) throws IOException
 {
- 
- int n;
- 
- int emp;
- 
- 
- Product2 p[]=new Product2[5];
- 
-for(int i=1;i<=3;i++)
- 
+int n;
+int p;
+product p[]= new product(n);
+for (int i=1;i<=n;i++)
 {
-  
- p[i]=new Product2();
-  
- p[i].read();
-  
- 
-}
- 
- 
-int min=1;
- 
-for(int i=1;i<=3;i++)
-
- {
-  
-  
-if(p[min].price>p[i].price)
- 
- {
-  
- min=i; 
-     
-}
-  
-}
-
-
-System.out.print("\n Product with lowest price  \n");
-
-p[min].Display();
-
-
-
+ product p[]= new product(i);
+ p.read(i);
  }
-
-
+ min=1;
+ for (int i=1;i<=n;i++)
+ {
+  if(  p[min].price<p[i].price)
+  {
+  system.out.print("\n the lowest price is:" );
+  p[min].display();
+  }
+  else
+  system.out.print("\n error");
+    }
+ }
+ 
+ class validation
+ {
+ void read() throws userException
+ {
+ scanner s= new scanner();
+ system.out.print("\n the length of item code" +p_code(length));
+ int p_code=s.nextInt();
+ system.out.print("\n the length of name is" +p_name(length));
+ string p_name= s.next();
+ }
+ 
+ void display() throws userException
+ {
+ system.out.print("\n the length of item code" +p_code(length));
+ system.out.print("\n the length of product name:"  char (+p_name(length)));
+ }
+ public static void main string(argu[])UserException
+ {
+ int m=4;
+ if (p_code(length)>m)
+ {
+ object obj= new object();
+ obj.read();
+ }
+ else
+ {
+ obj.show();
+ }
+if (p_name(length)<=15)
+{ 
+object.read();
 }
+else
+{
+obj.show(); 
+}
+class UserException implements Exception
+{
+system.out.print("\n the code length is:"+p_code(length));
+system.out.print("\n the name length is:"+p_name(length));
+obj.display();
+ }
+ }
+ }
+ 
