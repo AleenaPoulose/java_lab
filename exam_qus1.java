5b6ad84eb8fbeb4cc8b7bd938156c94bf84ea77d
AleenaPoulose <91655836+AleenaPoulose@users.noreply.github.com>
2021-10-06 12:28:25 +0530

    Create exam_qus1.java

---
+++ exam_qus1.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import java.util.scanner;
+
+class number
+{
+int num;
+
+void read() throws IOException
+{
+scanner sc= new scanner (system.in);
+system.out.print(" \n Enter the number:");
+int num = sc.nextInt()
+}
+void display() throws IOException
+{
+system.out.print("\n The number is:"+num);
+}
+public static void name string argu [] throws IOException
+{
+int i,l;
+system.out.print("enter the limit"+l);
+for (i=0;i<l;i++)
+ { 
+system.out.print("the reverse of the number is:" +rev(num));
+}
+}
